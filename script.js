3237cb30759678c7a055e85cfa091b1fa81dfe0c
isnendyankp <isnendyankp@gmail.com>
2023-07-02 09:45:35 +0700

    S21-203: setup Clear local Storage

--- script.js
+++ script.js
@@ -192,13 +192,11 @@ loadStorageBtn.addEventListener('click', () => {
   }
 });
 
-// // Clear Local Storage
-// clearStorageBtn.addEventListener('click', () => {
+// Clear Local Storage
+clearStorageBtn.addEventListener('click', () => {
 
-//   // Active Tool
-//   activeToolEl.textContent = 'Local Storage Cleared';
-//   setTimeout(switchToBrush, 1500);
-// });
+ 
+});
 
 // // Download Image
 // downloadBtn.addEventListener('click', () => {
